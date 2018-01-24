a47ab434e8e185ef9afdfc6c986023d40131c6f3
Cédric S <cedric.scys@gmail.com>
2018-01-24 16:02:09 +0100

    Création de constants.js

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+// Libellés des modules à afficher dans l'application. Ils doivent être utilisés lors de la création des voyages
+export const LOGEMENTS = 'Logements'
+export const TRANSPORTS = 'Transports'
+export const DEPENSES = 'Dépenses'
+export const ACTIVITES = 'Activités'
+export const DOCUMENTS = 'Documents'
+export const LISTES = 'Listes'
