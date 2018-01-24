c74473748aa17a99ff8e6a2bdee865b4c60dbb99
Cédric S <cedric.scys@gmail.com>
2018-01-24 16:11:21 +0100

    création des constantes pour les libellés des modules

---
+++ src/util.js
@@ -0,0 +1,30 @@
+import { LOGEMENTS, TRANSPORTS, DEPENSES, ACTIVITES, DOCUMENTS, LISTES } from '~/constants'
+
+/**
+ * Méthode renvoyant le libellé du module en fonction de son id
+ * @param {*} nIdModule id du libellé du module
+ */
+export function getModuleNameById (nIdModule) {
+  switch (nIdModule) {
+    case 0:
+      return LOGEMENTS
+
+    case 1:
+      return TRANSPORTS
+
+    case 2:
+      return DEPENSES
+
+    case 3:
+      return ACTIVITES
+
+    case 4:
+      return DOCUMENTS
+
+    case 5:
+      return LISTES
+
+    default :
+      return 'INCONNU'
+  }
+}
