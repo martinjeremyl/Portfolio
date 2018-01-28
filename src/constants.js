f0ad50f78dd5e0fc3eb7cc0fbfdaa96a16f5b7e1
Cédric S <cedric.scys@gmail.com>
2018-01-28 16:04:49 +0100

    constantes en majuscule

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,12 @@
 // Libellés des modules à afficher dans l'application. Ils doivent être utilisés lors de la création des voyages
-export const LOGEMENTS = 'Logements'
-export const TRANSPORTS = 'Transports'
-export const DEPENSES = 'Dépenses'
-export const ACTIVITES = 'Activités'
-export const DOCUMENTS = 'Documents'
-export const LISTES = 'Listes'
+export const LOGEMENTS = 'LOGEMENTS'
+
+export const TRANSPORTS = 'TRANSPORTS'
+
+export const DEPENSES = 'DEPENSES'
+
+export const ACTIVITES = 'ACTIVITES'
+
+export const DOCUMENTS = 'DOCUMENTS'
+
+export const LISTES = 'LISTES'
