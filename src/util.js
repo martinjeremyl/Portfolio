42993b141d5a84dfb9df2e020dd69cb71a48aa78
m5r <me@mokhtarmial.com>
2018-03-08 16:22:05 +0100

    fix travis

--- src/util.js
+++ src/util.js
@@ -47,6 +47,6 @@ const formatter = Intl.NumberFormat('fr-FR', {
   style: 'currency',
   currency: 'EUR',
   minimumFractionDigits: 2
-});
+})
 
 export const formatCurrency = amount => formatter.format(amount)
