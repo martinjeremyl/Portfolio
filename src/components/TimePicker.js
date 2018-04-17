31f31464beebb45df59b86cfc813bd3469d2f31e
martinjeremyl <32765322+martinjeremyl@users.noreply.github.com>
2018-04-17 15:03:46 +0200

    227 add selectpicker, datepicker and fix bootstrap css issues (#241)
    * add bootstrap, jquery and popper.js, modif index.js
    
    * add gitignore modifications
    
    * delete old files
    
    * fix multiple issues
    
    * add datepicker, timepicker and fix bootstrap css issues
    
    * delete examples

---
+++ src/components/TimePicker.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDatePicker from 'react-datepicker'
+import moment from 'moment'
+
+import 'react-datepicker/dist/react-datepicker.css'
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+class TimePicker extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment()
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  render () {
+    return <ReactDatePicker
+      selected={this.state.startDate}
+      onChange={this.handleChange}
+      showTimeSelect
+      showTimeSelectOnly
+      timeIntervals={15}
+      dateFormat='LT'
+      timeCaption='Time'
+    />
+  }
+}
+export default TimePicker
