52035b304fd0e183b8d7a145863d28442cf78fa6
Mokhtar <mokhtar.mial@gmail.com>
2018-04-17 15:12:17 +0200

    date display component (#246)

---
+++ src/components/DateDisplay.js
@@ -0,0 +1,31 @@
+import React, {Component, Fragment} from 'react'
+
+const dateFormatter = new Intl.DateTimeFormat('fr', {
+  hour12: false,
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric'
+})
+
+const timeFormatter = new Intl.DateTimeFormat('fr', {
+  hour12: false,
+  hour: 'numeric',
+  minute: 'numeric'
+})
+
+export class DateDisplay extends Component {
+  render () {
+    const {date} = this.props
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+        <div>
+          {dateFormatter.format(date)}
+        </div>
+        <div>
+          {timeFormatter.format(date)}
+        </div>
+      </div>
+    )
+  }
+}
