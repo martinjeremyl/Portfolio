5a6434922f1c8069661eceb2bbd83d9f9cdf1079
Mokhtar <mokhtar.mial@gmail.com>
2018-04-19 17:15:48 +0200

    fix input textfield (#304)

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import TextField from 'material-ui/TextField'
 
 const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap'
-  },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200
-  },
-  menu: {
     width: 200
   }
 })
@@ -27,13 +19,9 @@ class Input extends React.Component {
   }
   render () {
     return (
-      <TextField {...this.props} />
+      <TextField className={this.props.classes.textField} />
     )
   }
-}
-
-TextField.propTypes = {
-  classes: PropTypes.object.isRequired
 }
 
 export default withStyles(styles)(Input)
