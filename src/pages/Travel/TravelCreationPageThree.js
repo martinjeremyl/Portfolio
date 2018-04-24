27a915def8e84323360748e5308393bb7f6444fd
maxime blanc <maximeblanc.dev@gmail.com>
2018-04-24 17:22:07 +0200

    add avatart

--- src/pages/Travel/TravelCreationPageThree.js
+++ src/pages/Travel/TravelCreationPageThree.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
 
+import Avatar from '../../components/Avatar'
 import Input from '../../components/Input'
 
 const MAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -32,10 +33,12 @@ class TravelCreationPage extends Component {
           (participant, key) =>
             key === 0 ? (
               <div key={key}>
+                <Avatar />
                 <Input value={participant} disabled />
               </div>
             ) : (
               <div key={key}>
+                <Avatar />
                 <Input value={participant} />
               </div>
             )
