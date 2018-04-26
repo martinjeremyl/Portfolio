6fd930cdbca6ec1d5818eb27d3c8ada1c5c85c52
Quentin Reboli <reboli.quentin@gmail.com>
2018-04-26 13:44:57 +0200

    remove back button on travel page and diplay on other (#349)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
-import { MuiThemeProvider, createMuiTheme } from 'material-ui'
+
+import BackButton from './BackButton'
 
 const routesTitle = {
   '/login': 'Connexion',
@@ -28,37 +29,33 @@ const styles = {
   }
 }
 
-const materialTheme = createMuiTheme({
-  overrides: {
-    MuiPaper: {
-      root: {
-        background: 'linear-gradient(to left, #F2BF95, #E45C55)',
-        textAlign: 'center'
-      }
-    }
-  }
-})
-
 @inject('routingStore')
 @observer
 class Header extends Component {
   render () {
-    const { classes, renderRightButton, title } = this.props
+    const { classes, renderRightButton, routingStore, title } = this.props
 
     return (
       <div className={classes.root}>
-        <MuiThemeProvider theme={materialTheme}>
-          <AppBar position='static'>
-            <Toolbar disableGutters>
-              <Typography variant='title' color='inherit' className={classes.flex}>
-                {title !== undefined
+        <AppBar position='static'>
+          <Toolbar disableGutters>
+            {
+              this.props.routingStore.location.pathname === '/travels'
+                ? ''
+                : <BackButton onClick={routingStore.goBack} />
+            }
+            <Typography variant='title' color='inherit' className={classes.flex}>
+              {
+                title !== undefined
                   ? title
-                  : routesTitleProxy[this.props.routingStore.location.pathname]}
-              </Typography>
-              {renderRightButton && renderRightButton()}
-            </Toolbar>
-          </AppBar>
-        </MuiThemeProvider>
+                  : routesTitleProxy[this.props.routingStore.location.pathname]
+              }
+            </Typography>
+            {
+              renderRightButton && renderRightButton()
+            }
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
