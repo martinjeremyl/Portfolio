3226f5b81d2fa941de4d6d95c5c2337c1783c48d
Cédric S <cedric.scys@gmail.com>
2018-05-22 14:54:48 +0200

    Align mail saisis

--- src/pages/Travel/TravelCreationPageThree.js
+++ src/pages/Travel/TravelCreationPageThree.js
@@ -33,21 +33,47 @@ class TravelCreationPage extends Component {
   render () {
     const { travelStore } = this.props
     const { newEmail } = this.state
+    const styles = {
+      fullContainer: {
+        padding: '20px'
+      },
+      personContainer: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignContent: 'center'
+      },
+      Avatar: {
+        flewGrow: '1'
+      },
+      myMail: {
+        flexGrow: '5'
+      },
+      newMail: {
+        flexGrow: '4'
+      },
+      deletePerson: {
+        flexGrow: '1'
+      },
+      newPersonLine: {
+        marginTop: '20px'
+      }
+    }
 
     return (
-      <div>
+      <div style={styles.fullContainer}>
         {travelStore.participants.map(
           (participant, key) =>
             key === 0 ? (
-              <div key={key}>
-                <Avatar style={{ display: 'inline-block' }} />
-                <Input value={participant} disabled />
+              <div key={key} style={styles.personContainer}>
+                <Avatar style={styles.Avatar} />
+                <Input value={participant} style={styles.myMail} disabled />
               </div>
             ) : (
-              <div key={key}>
-                <Avatar style={{ display: 'inline-block' }} />
-                <Input value={participant} />
+              <div key={key} style={styles.personContainer}>
+                <Avatar style={styles.avatar} />
+                <Input value={participant} style={{alignItems: 'center'}} />
                 <button
+                  style={styles.deletePerson}
                   onClick={() => {
                     travelStore.updateTravelCreation(
                       'participants',
@@ -60,23 +86,26 @@ class TravelCreationPage extends Component {
               </div>
             )
         )}
-        <Input onChange={this.handleNewEmail} value={newEmail} />
-        <button
-          onClick={() => {
-            if (MAIL_REGEX.test(newEmail)) {
-              travelStore.updateTravelCreation('participants', [
-                ...travelStore.participants,
-                newEmail
-              ])
+        <div style={styles.newPersonLine}>
+          <Input onChange={this.handleNewEmail} value={newEmail} />
+          <button
+            onClick={() => {
+              if (MAIL_REGEX.test(newEmail)) {
+                travelStore.updateTravelCreation('participants', [
+                  ...travelStore.participants,
+                  newEmail
+                ])
 
-              this.setState({
-                newEmail: ''
-              })
-            }
-          }}
-        >
-          Ajouter
-        </button>
+                this.setState({
+                  newEmail: ''
+                })
+              }
+            }}
+          >
+            Ajouter
+          </button>
+        </div>
+
       </div>
     )
   }
