1c1faa1a71075513ca2036952dbe4fb646877bde
Mokhtar <mokhtar.mial@gmail.com>
2018-05-25 11:19:08 +0200

    * fix weird appstore error on Link (#385)
    * installed react-spring to animate stuff
    * added travel detail edit menu front

--- src/pages/Travel/TravelDetailPage.js
+++ src/pages/Travel/TravelDetailPage.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import Button from 'material-ui/Button'
 
 import TravelMenuDrawer from '../../components/TravelMenuDrawer'
 import Navbar from '../../components/Navbar'
 import Header from '../../components/Header'
+import TravelDetailEditMenu from '../../components/TravelDetailEditMenu'
 
 @inject('travelStore')
 @observer
 class TravelDetailPage extends Component {
   state = {
-    travelMembers: []
+    travelMembers: [],
+    isEditTravelMenuOpen: false
   }
+
+  openEditTravelMenu = () => this.setState({ isEditTravelMenuOpen: true })
+
+  closeEditTravelMenu = () => this.setState({ isEditTravelMenuOpen: false })
 
   async componentDidMount () {
     const { travelStore, match } = this.props
@@ -18,26 +25,36 @@ class TravelDetailPage extends Component {
     travelStore.setCurrentTravelId(match.params.id)
   }
 
-  renderMenuButton () {
-    // Render the burger menu icon with the swapeable drawer
-    return (<TravelMenuDrawer />)
-  }
-
   render () {
     const { travelStore } = this.props
 
     return (
       <div>
-        <Header renderLeftButton={this.renderMenuButton} />
+        <Header renderLeftButton={() => <TravelMenuDrawer />} />
         <Navbar />
-        <h1>{travelStore.travel.name}</h1>
-        <div>Début : {travelStore.travel.startDate}</div>
-        <div>Fin : {travelStore.travel.endDate}</div>
-        <div>Image : {travelStore.travel.image}</div>
-        <div>Description : {travelStore.travel.description}</div>
-        <div>Participants : <ul>{travelStore.travel.members.map(
-          member => <li key={member.id}>{member.surname} {member.name}</li>
-        )}</ul></div>
+        <h1>{travelStore.travel && travelStore.travel.name}</h1>
+        <Button onClick={this.openEditTravelMenu}>Edit Travel</Button>
+        <div>Début : {travelStore.travel && travelStore.travel.startDate}</div>
+        <div>Fin : {travelStore.travel && travelStore.travel.endDate}</div>
+        <div>Image : {travelStore.travel && travelStore.travel.image}</div>
+        <div>Description : {travelStore.travel && travelStore.travel.description}</div>
+        <div>Participants :
+          <ul>
+            {
+              travelStore.travel && travelStore.travel.members.map(
+                member => (
+                  <li key={member.id}>
+                    {member.surname} {member.name}
+                  </li>
+                )
+              )
+            }
+          </ul>
+        </div>
+        <TravelDetailEditMenu
+          isEditTravelMenuOpen={this.state.isEditTravelMenuOpen}
+          closeEditTravelMenu={this.closeEditTravelMenu}
+        />
       </div>
     )
   }
